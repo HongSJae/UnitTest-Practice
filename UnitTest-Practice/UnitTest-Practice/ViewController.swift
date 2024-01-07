1f30751b1179bc6e4c1dd094250f430aba41143f
HongSJae <gtw030488@gmail.com>
2024-01-08 08:56:59 +0900

    🧩 :: file init

---
+++ UnitTest-Practice/UnitTest-Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UnitTest-Practice
+//
+//  Created by 홍승재 on 1/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
